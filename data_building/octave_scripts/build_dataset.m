64a1f54eac2e21c84c5a3f30e1d3853dd532f8fd
Valentin Andrei <am_valentin@yahoo.com>
2018-09-27 20:45:52 +0000

    Latest changes to linux branch.

--- data_building/octave_scripts/build_dataset.m
+++ data_building/octave_scripts/build_dataset.m
@@ -8,8 +8,8 @@ pkg load ltfat
 
 % ------------------------------------------------------------------------------
 
-v_dir_database  = '/home/valentin_m_andrei/datasets/LibriSpeech/dev-clean/*';
-n_max_speaker_directories = 40;
+v_dir_database  = '/home/valentin_m_andrei/datasets/LibriSpeech/test-clean/*';
+n_max_speaker_directories = 50;
 
 % ------------------------------------------------------------------------------
 
@@ -18,8 +18,8 @@ frame_ms            = 1000;
 frame_inc_ms        = 50;
 n_classes           = 4;
 n_max_speakers      = 4;
-n_samples_per_count = 150000;
-n_block_size        = 50000
+n_samples_per_count = 25000;
+n_block_size        = 25000
 with_reverb         = 0;
 count_speakers      = 1;
 b_add_square_feats  = 0;
