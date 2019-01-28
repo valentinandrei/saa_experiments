ba6fa24a5afd2961b7cefaddf45d66c16d1a079f
Valentin Andrei <am_valentin@yahoo.com>
2019-01-27 18:53:00 -0800

    Fixed some bugs.

--- data_building/octave_scripts/run_perception_experiment_w_replay.m
+++ data_building/octave_scripts/run_perception_experiment_w_replay.m
@@ -10,67 +10,6 @@ v_frame_ms      = [5000, 2000, 1000, 500];
 % -------------------
 
 n_total_samples = n_max_speakers * n_durations * n_samples;
-m_norep_results = zeros(n_total_samples, 3);
-v_sample_ids = randperm(n_total_samples) - 1;
-
-% Load the Mixtures (cMixturesNoReplay)
-printf("Se incarca fisierul cu inregistrari.\n");
-load mixtures_no_replay.mat
-
-% Start the test
-printf("In prima parte a experimentului, nu veti putea reasculta inregistrarile.\n");
-printf("Apasati orice tasta pentru a incepe experimentul.\n");
-fflush(stdout);
-pause();
-
-% DEBUG
-% n_total_samples = 5;
-
-% Play the sounds
-for i = 1 : n_total_samples
-  n_index = v_sample_ids(i);
-  n_duration_id = 1 + floor(n_index / n_max_speakers / n_samples);
-  n_sample_id = 1 + mod(n_index, n_max_speakers * n_samples);
-  
-  % Extract Mixture
-  v_mixture = cMixturesNoReplay{n_duration_id, 1}(n_sample_id, :);
-  
-  % Extract Label
-  v_label = cMixturesNoReplay{n_duration_id, 2}(n_sample_id, :);
-  
-  % Play Sound and Ask For Count
-  n_claimed_speaker_count = 0;
-  soundsc(v_mixture, fs); 
-    
-  printf("\n%d/%d Cati vorbitori simultan ati putut numara? ", i, n_total_samples);
-  printf("Introduceti un numar de la 1 la 4, inclusiv.\n");
-  fflush(stdout);
-
-  while (n_claimed_speaker_count == 0)
-    try
-      n_claimed_speaker_count = menu("Cati vorbitori ati putut numara?", "1", "2", "3", "4");
-      if ((n_claimed_speaker_count < 1) || (n_claimed_speaker_count > 4))
-        n_claimed_speaker_count = 0;
-      end
-    catch
-      n_claimed_speaker_count = 0;
-    end_try_catch
-  end
-  
-  % Save answer
-  m_norep_results(i, 1) = v_frame_ms(n_duration_id);
-  m_norep_results(i, 2) = n_claimed_speaker_count;
-  m_norep_results(i, 3) = find(v_label, 1);
-  
-end
-
-f_out = sprintf("no_replay_%d.mat", listener_id);
-save(f_out, "m_norep_results");
-
-% ----------------
-% Run Replay Phase
-% ----------------
-
 m_rep_results = zeros(n_total_samples, 3);
 v_sample_ids = randperm(n_total_samples) - 1;
 
@@ -104,11 +43,10 @@ for i = 1 : n_total_samples
    
     printf("\n%d/%d Cati vorbitori simultan ati putut numara? ", i, n_total_samples);
     printf("Introduceti un numar de la 1 la 4, inclusiv.\n");
-    printf("Daca doriti sa reascultati, introduceti orice alt numar.\n\n");
     fflush(stdout);
  
     try
-      n_claimed_speaker_count = menu("Cati vorbitori ati putut numara?", "1", "2", "3", "4", "Repetare");
+      n_claimed_speaker_count = menu("Cati vorbitori ati auzit: ", "1", "2", "3", "4", "Repetare");
       if ((n_claimed_speaker_count < 1) || (n_claimed_speaker_count > 4))
         n_claimed_speaker_count = 0;
       elseif (length(n_claimed_speaker_count) == 0)
